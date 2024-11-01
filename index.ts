093bb9737c97f700c65bbbf6c9148ad863567b2a
ESSTX <66343496+ESSTX@users.noreply.github.com>
2024-11-01 17:18:07 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -21,10 +21,14 @@ export async function getVibrantColors(imageUrl: string, maxColors: number = 5):
                 const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
                 const colorMap: Map<string, { count: number; brightness: number }> = new Map();
 
+                let isGrayscale = true;
+
                 for (let i = 0; i < imageData.data.length; i += 4) {
                     const [r, g, b] = imageData.data.slice(i, i + 3);
 
-                    if (r === 0 && g === 0 && b === 0) continue;
+                    if ((r === 0 && g === 0 && b === 0) || (r > 200 && g > 200 && b > 200) || (Math.abs(r - g) <= 10 && Math.abs(r - b) <= 10 && Math.abs(g - b) <= 10)) continue;
+
+                    isGrayscale = false;
 
                     const hexColor = `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()}`;
                     const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
@@ -37,11 +41,14 @@ export async function getVibrantColors(imageUrl: string, maxColors: number = 5):
                     }
                 }
 
+                if (isGrayscale) {
+                    return reject(new Error("Image is fully grayscale"));
+                }
+
                 const vibrantColors = Array.from(colorMap.entries())
                     .sort((a, b) => b[1].brightness - a[1].brightness || b[1].count - a[1].count)
                     .slice(0, maxColors)
                     .map(color => color[0]);
-
                 resolve(vibrantColors);
             } catch {
                 reject(new Error("Canvas was tainted by cross-origin data"));
